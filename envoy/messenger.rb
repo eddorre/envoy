18625346a6a94465cafb47bfa577e657dcb29a20
Carlos Rodriguez <carlos@eddorre.com>
2010-10-17 16:13:33 -0700

    Transport method now changes transport parameter to a string and capitalizes it - now you can pass in symbols as the transport name. Added give_messages block which takes a block and adds messages. Added give_message method which added Message object to the Messenger's messages

--- envoy/messenger.rb
+++ envoy/messenger.rb
@@ -11,17 +11,27 @@ module Envoy
     end
 
     def transport(transport, options = {})
-      transport_instance = Module.const_get("Envoy").const_get(transport).new(options)
+      transport_instance = Module.const_get("Envoy").const_get(transport.to_s.capitalize).new(options)
       self.transports << transport_instance
     end
 
     def deliver_messages
       self.messages.each do |message|
         self.transports.each do |transport|
-          transport.send_messaage(message)
+          transport.send_message(message)
         end
       end
     end
+
+    def give_messages(&block)
+      yield self if block_given?
+    end
+
+    def give_message(message_options = {})
+      message_options.symbolize_keys!
+      self.messages << Envoy::Message.new(message_options[:name], message_options[:subject], message_options[:body])
+    end
+    alias :message :give_message
 
     def method_missing(method, *args)
       message = self.messages.select { |message| message.name == method.to_s.gsub('deliver_', '') }.first
