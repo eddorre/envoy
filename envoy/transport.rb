20cc145d749e53634ee444707bb5845c6da902d1
Carlos Rodriguez <carlos@eddorre.com>
2010-10-18 22:48:15 -0700

    Use SSL is now a parameter to the Campfire transport, host and username are instance variables in the Mail transport, changing host.to_sym and see if it matches :sendmail, sender can now override default sender

--- envoy/transport.rb
+++ envoy/transport.rb
@@ -14,20 +14,20 @@ module Envoy
   end
 
   class Campfire < Transport
-    attr_accessor :host, :username, :password, :room
+    attr_accessor :host, :account, :token, :room, :use_ssl
 
     def initialize(options = {})
       super
       @room = options[:room]
       @username = options[:account]
       @password = options[:token]
+      @use_ssl = options[:token]
     end
 
     def send_message(message)
-      Broach.settings = { 'account' => self.username, 'token' => self.password }
-      Broach.speak(self.room, message.body)
+      Broach.settings = { 'account' => self.username, 'token' => self.password, 'use_ssl' => self.use_ssl }
+      Broach.speak(self.room, message.body || message.subject)
     end
-
   end
 
   class Mail < Transport
@@ -35,6 +35,8 @@ module Envoy
 
     def initialize(options = {})
       super
+      @host = options[:host]
+      @username = options[:username]
       @sender = options[:sender]
       @to = options[:to]
       @port = options[:port] || 25
@@ -43,14 +45,17 @@ module Envoy
     end
 
     def send_message(message)
-      if @host == 'sendmail' || :sendmail
-        Pony.mail(:from => 'Envoy Messenger <envoymessenger@localhost>', :to => (@to.is_a?(String) ? @to : @to.join(',')),
+      if self.host.to_sym == :sendmail
+        Pony.mail(:from => (@sender.nil? ? 'Envoy Messenger <envoymessenger@localhost>' : @sender),
+          :to => (@to.is_a?(String) ? @to : @to.join(',')),
           :via => :sendmail, :body => message.body || message.subject, :subject => message.subject)
       else
-        Pony.mail(:from => 'Envoy Messenger <envoymessenger@localhost>', :to => (@to.is_a?(String) ? @to : @to.join(',')), :via => :smtp, :via_options => {
-          :address => @host, :port => @port, :enable_starttls_auto => @ssl, :user_name => :username,
-          :password => @password, :authentication => @authentication, :body => message.body || message.subject, :subject => message.subject
-        })
+        Pony.mail(:from => (@sender.nil? ? 'Envoy Messenger <envoymessenger@localhost>' : @sender),
+          :to => (@to.is_a?(String) ? @to : @to.join(',')), :via => :smtp, :via_options =>
+            {
+              :address => @host, :port => @port, :enable_starttls_auto => @ssl, :user_name => @username,
+              :password => @password, :authentication => @authentication, :body => message.body || message.subject, :subject => message.subject
+            })
       end
     end
   end
