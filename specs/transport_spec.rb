0c35714d8d30cd6c1f35abeec9d16e7d9b1e3c49
Carlos Rodriguez <carlos@eddorre.com>
2010-11-12 21:16:27 -0800

    Fakeweb is now a requirement for running specs as the Webhook specs require it

--- specs/transport_spec.rb
+++ specs/transport_spec.rb
@@ -1,9 +1,10 @@
 require 'envoy'
+require 'fakeweb'
 include Envoy
 
 describe Transport do
   before(:each) do
-    @message = Message.new('Test message', 'this is a test message' )
+    @message = Message.new('Test message', 'this is a test message')
   end
 
   describe "Campfire Transport" do
@@ -64,6 +65,23 @@ describe Transport do
       :to => 'carlos@eddorre.com', :body => @message.subject, :subject => @message.subject)
 
       @transport.send_message(@message)
+    end
+  end
+
+  describe "Webhook Transport" do
+    before(:each) do
+      @transport = Envoy::Webhook.new
+      @transport.url = 'http://foo.com/'
+    end
+
+    it "should send a message to the webhook URL and return true when the service returns 200" do
+      FakeWeb.register_uri(:post, 'http://foo.com', :body => "OK", :status => ["200", "OK"])
+      @transport.send_message(@message).should == true
+    end
+
+    it "should send a messaged to the webhook url and return false when the service doesn't return a 200" do
+      FakeWeb.register_uri(:post, 'http://foo.com', :body => "Nothing here", :status => ["404", "Not Found"])
+      @transport.send_message(@message).should == false
     end
   end
 end
