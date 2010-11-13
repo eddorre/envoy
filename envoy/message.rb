a56dcddcc1d6ca512161dd536d339f16ee44202c
Carlos Rodriguez <carlos@eddorre.com>
2010-11-12 21:15:13 -0800

    Added optional options hash to message

--- envoy/message.rb
+++ envoy/message.rb
@@ -1,11 +1,12 @@
 module Envoy
   class Message
-    attr_accessor :name, :body, :subject
+    attr_accessor :name, :body, :subject, :options
 
-    def initialize(name, subject, body = nil)
+    def initialize(name, subject, body = nil, options = {})
       self.name = name
       self.body = body
       self.subject = subject
+      self.options = options
     end
   end
 end
