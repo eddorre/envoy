5b7d87cdd9a9f5aefbdf423558f7724297820352
Carlos Rodriguez <carlos@eddorre.com>
2010-11-12 20:39:38 -0800

    Removing dependency on fakeweb

--- specs/transport_spec.rb
+++ specs/transport_spec.rb
@@ -1,70 +1,69 @@
 require 'envoy'
-require 'fakeweb'
 include Envoy
 
 describe Transport do
   before(:each) do
     @message = Message.new('Test message', 'this is a test message' )
   end
-  
-  describe "Campfire Transport" do  
+
+  describe "Campfire Transport" do
     it "should send a message through broach" do
       transport = Campfire.new(:account => 'foo', :token => 'bar', :room => 'meep')
       Broach.should_receive(:speak).with(transport.room, @message.subject)
       transport.send_message(@message)
     end
   end
-  
+
   describe "Mail Transport" do
     before(:each) do
       @transport = Envoy::Mail.new
       @transport.to = 'carlos@eddorre.com'
     end
-    
+
     it "should send a message through pony" do
       @transport.host = :sendmail
       Pony.should_receive(:mail)
       @transport.send_message(@message)
     end
-    
+
     it "should send a message through pony with sendmail" do
       @transport.host = :sendmail
 
-      
-      Pony.should_receive(:mail).with(:via => :sendmail, :from => 'Envoy Messenger <envoymessenger@localhost>', 
+
+      Pony.should_receive(:mail).with(:via => :sendmail, :from => 'Envoy Messenger <envoymessenger@localhost>',
       :to => 'carlos@eddorre.com', :body => @message.subject, :subject => @message.subject)
-      
+
       @transport.send_message(@message)
     end
-    
+
     it "should send a message through pony using SMTP" do
       @transport.host = 'mail.eddorre.com'
-      
+
       Pony.should_receive(:mail).with(:from => 'Envoy Messenger <envoymessenger@localhost>', :to => 'carlos@eddorre.com', :via => :smtp, :via_options => { :address => @transport.host,
         :port => 25, :enable_starttls_auto => false, :user_name => nil, :password => nil, :authentication => nil,
-        :body => @message.subject, :subject => @message.subject })      
-        
+        :body => @message.subject, :subject => @message.subject })
+
       @transport.send_message(@message)
     end
-    
+
     it "should send a message through pony with multiple recipients" do
       @transport.to = ['carlos@eddorre.com', 'hello@eddorre.com']
       @transport.host = :sendmail
-      
-      Pony.should_receive(:mail).with(:via => :sendmail, :from => 'Envoy Messenger <envoymessenger@localhost>', 
+
+      Pony.should_receive(:mail).with(:via => :sendmail, :from => 'Envoy Messenger <envoymessenger@localhost>',
       :to => 'carlos@eddorre.com,hello@eddorre.com', :body => @message.subject, :subject => @message.subject)
-      
+
       @transport.send_message(@message)
     end
-    
+
     it "should send a message through pony with a set sender" do
       @transport.host = :sendmail
       @transport.sender = 'carlos@eddorre.com'
-      
-      Pony.should_receive(:mail).with(:via => :sendmail, :from => 'carlos@eddorre.com', 
+
+      Pony.should_receive(:mail).with(:via => :sendmail, :from => 'carlos@eddorre.com',
       :to => 'carlos@eddorre.com', :body => @message.subject, :subject => @message.subject)
-      
-      @transport.send_message(@message)      
+
+      @transport.send_message(@message)
     end
   end
 end
